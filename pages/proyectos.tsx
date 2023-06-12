8dc05642d3d4c153f7e8a4eee2809e0ddc39f217
patriciotourne <patriciotourne@estudiantesorganizados.ar>
2023-06-12 20:49:33 -0300

    async request de proyectos funcionando!

--- pages/proyectos.tsx
+++ pages/proyectos.tsx
@@ -7,12 +7,14 @@ import { Proyecto } from "./types";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Proyectos() {
-  //const [projects, setProjects] = useState<Proyecto[]>([]);
+  const [projects, setProjects] = useState<Proyecto[]>([]);
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedProject, setSelectedProject] = useState<Proyecto | undefined>(
+    undefined
+  );
 
-  /*
   const getProjects = async () => {
-    //Proyectos de prueba hasta que tengamos conexion con el back-end
+    // Proyectos de prueba hasta que tengamos conexión con el back-end
     const projects: Proyecto[] = [
       {
         id: "1",
@@ -42,43 +44,18 @@ export default function Proyectos() {
         costo_proyecto: 150000,
       },
     ];
+
     setProjects(projects);
   };
-  */
-  const projects: Proyecto[] = [
-    {
-      id: "1",
-      nombre: "Primer Proyecto",
-      fecha_inicio: new Date(),
-      fecha_fin: new Date(),
-      estado: "En curso",
-      horas_consumidas: 15,
-      costo_proyecto: 50000,
-    },
-    {
-      id: "2",
-      nombre: "Segundo Proyecto",
-      fecha_inicio: new Date(),
-      fecha_fin: new Date(),
-      estado: "En curso",
-      horas_consumidas: 8,
-      costo_proyecto: 70000,
-    },
-    {
-      id: "3",
-      nombre: "Tercer Proyecto",
-      fecha_inicio: new Date(),
-      fecha_fin: new Date(),
-      estado: "Finalizado",
-      horas_consumidas: 40,
-      costo_proyecto: 150000,
-    },
-  ];
-  /*
+
   useEffect(() => {
     getProjects();
   }, []);
-*/
+
+  useEffect(() => {
+    setSelectedProject(projects[selectedIndex]);
+  }, [projects, selectedIndex]);
+
   return (
     <div className="flex flex-row h-full">
       <div className="flex flex-fill h-full flex-col p-4 bg-white w-30">
@@ -104,7 +81,7 @@ export default function Proyectos() {
             : "Aún no hay proyectos creados. Seleccione agregar para crear uno nuevo"}
         </div>
       </div>
-      <ProjectSideBar project={projects[selectedIndex]} />
+      {selectedProject && <ProjectSideBar project={selectedProject} />}
     </div>
   );
 }
